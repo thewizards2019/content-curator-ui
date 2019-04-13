243c918c136bad28e15c340ad1644af0446deb61
Connor Bradley <cbradley@statestreet.com>
2019-04-13 14:33:00 +0100

    Added components for each tab

--- content-curator-ui/src/app/app.module.ts
+++ content-curator-ui/src/app/app.module.ts
@@ -7,13 +7,19 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { MaterialModule } from './material/material.module';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { HomeComponent } from './home/home.component'
-import { LayoutComponent } from './layout/layout.component'
+import { LayoutComponent } from './layout/layout.component';
+import { FeedComponent } from './feed/feed.component';
+import { TrainComponent } from './train/train.component';
+import { SettingsComponent } from './settings/settings.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    LayoutComponent
+    LayoutComponent,
+    FeedComponent,
+    TrainComponent,
+    SettingsComponent
   ],
   imports: [
     BrowserModule,
