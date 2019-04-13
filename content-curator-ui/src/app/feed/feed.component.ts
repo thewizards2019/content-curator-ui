a9f0afc4ad4435eea879275e2131dd53d17c4be3
Connor Bradley <cbradley@statestreet.com>
2019-04-13 13:00:50 +0100

    Added Feed component for feed page:

---
+++ content-curator-ui/src/app/feed/feed.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-feed',
+  templateUrl: './feed.component.html',
+  styleUrls: ['./feed.component.sass']
+})
+export class FeedComponent implements OnInit {
+
+  hero = 'Hi there'
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
