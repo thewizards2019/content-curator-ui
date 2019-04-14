f0a868d583e17bb56e355ae3f1a43e6940072879
Connor Bradley <cbradley@statestreet.com>
2019-04-14 11:19:49 +0100

    Added frosted style

--- src/app/api/api.service.ts
+++ src/app/api/api.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http'
 import { Post } from 'src/app/models/Post.model'
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators'
+import { TrainingPost } from '../models/TrainingPost.model';
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +11,8 @@ import { Observable } from 'rxjs';
 export class ApiService {
   dataCuratorUri: string = 'http://127.0.0.1/'
   feedUri: string = 'http://127.0.0.1:5000/feed/'
-  private allData: string = 'http://localhost:8080/all';
+  private allData: string = '/api/feed';
+  private tagDataUrl: string = '/api/tag/';
 
   public userSettings = {
     sentimentValue: 0.5,
@@ -24,8 +27,8 @@ export class ApiService {
   //   this.httpClient.post(url, null)
   // }
 
-  getAllData(): Observable<{ CONTENT: string, SENTIMENT: string, PROFANITY: string }> {
-    return this.httpClient.get(this.allData) as Observable<{ CONTENT: string, SENTIMENT: string, PROFANITY: string }>;
+  getAllData(): Observable<Post[]> {
+    return this.httpClient.get(this.allData) as Observable<Post[]>;
   }
 
   getUserSettings() {
@@ -34,11 +37,17 @@ export class ApiService {
     return this.userSettings;
   }
 
+  tagData(UUID: number, showMore: boolean){
+    var urlToCall = this.tagDataUrl + UUID + "/" + showMore;
+    console.log("Calling..." + urlToCall)
+    this.httpClient.post(urlToCall, null)
+      .subscribe();
+  }
+
   setUserSettings(settingType, settingValue) {
     // tomorrow we will publish to kafka via tagging
     // set them in memory for now
     this.userSettings[settingType] = settingValue;
     console.log('Updated User Settings', this.userSettings);
   }
-
 }
